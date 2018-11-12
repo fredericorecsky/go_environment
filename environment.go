928d812241ee83e2341918aed04e6d27e8cf3a43
Frederico Recsky <frederico.recsky@booking.com>
2018-11-12 08:17:48 +0100

    initial commit

---
+++ environment.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"google.golang.org/appengine"
+
+)
+
+
+func Handler (w http.ResponseWriter, r *http.Request) {
+
+	cloudengine :=  appengine.IsDevAppServer()
+	appserver := appengine.IsAppEngine()
+
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	fmt.Fprintf(w, "Dev %t, App %t \n", cloudengine, appserver)
+}
+
+func main() {
+
+	http.HandleFunc("/", Handler)
+	log.Fatal(http.ListenAndServe( ":8080", nil ))
+
+}
